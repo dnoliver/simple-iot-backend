e1309cae712bcec5ced35136f20659ae749f2d08
Nicolas Oliver <dario.n.oliver@intel.com>
2016-06-13 16:27:22 -0300

    add consumer producer and controller page

--- socket/socket.js
+++ socket/socket.js
@@ -4,21 +4,40 @@ var debug = require('debug')('simple-iot-backend:socket');
 var socket = {
 	init: function (server) {
 		debug("SocketServer", "init");
-		
+
+		// This are the available rooms
+		var Rooms = ["producer", "consumer", "actuator", "controller"];
+
 		// This is the SocketIO server, allows comunication with WebClients
 		this.SocketIOServer = SocketIO.listen(server);
 
 		this.SocketIOServer.sockets.on('connection', onSocketClientConnection);
 
+		var Sockets = this.SocketIOServer.sockets;
+		
 		function onSocketClientConnection(client) {
 			debug('onSocketClientConnection');
-			client.on('data', onSocketClientData.bind(client));
-		}
-		
-		function onSocketClientData(data) {
-			this.broadcast.emit('data', data);
+			// join the client to a room
+			client.on('room', function (room) {
+				debug('onSocketClientRoom', room);
+
+				if (Rooms.indexOf(room) >= 0) {
+					debug('SocketClientJoinRoom', room);
+					client.join(room);
+				} else {
+					debug('Unknown Room', room);
+				}
+			});
+			// emit messages from producer
+			client.on('producer', function (data) {
+				Sockets.in('consumer').emit('producer', data);
+			});
+			// emit messages from controller
+			client.on('controller', function (data) {
+				Sockets.in('actuator').emit('controller', data);
+			});
 		}
 	}
-}
+};
 
 module.exports = socket;
